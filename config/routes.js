3e7aadb36440c26bab95bb2c793899983c21d0a3
Wizek <123.wizek@gmail.com>
2012-05-30 14:49:37 +0200

    Created User CRUD
    rw g crud user username password email reputation:number createdAt:date
    updatedAt:date geoX:number geoY:number --tpl jade

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 exports.routes = function (map) {
+    map.resources('needs');
+    map.resources('users');
 
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
--- db/schema.js
+++ db/schema.js
@@ -21,3 +21,29 @@
 
 */
 
+var User = describe('User', function () {
+    property('username', String);
+    property('password', String);
+    property('email', String);
+    property('reputation', Number);
+    property('createdAt', Date);
+    property('updatedAt', Date);
+    property('geoX', Number);
+    property('geoY', Number);
+});var User = describe('User', function () {
+    property('username', String);
+    property('password', String);
+    property('email', String);
+    property('reputation', Number);
+    property('createdAt', Date);
+    property('updatedAt', Date);
+    property('geoX', Number);
+    property('geoY', Number);
+});var Need = describe('Need', function () {
+    property('NeederID', String);
+    property('FulfillerID', String);
+});var Need = describe('Need', function () {
+    property('NeederID', String);
+    property('FulfillerID', String);
+    property('Needee', String);
+});
